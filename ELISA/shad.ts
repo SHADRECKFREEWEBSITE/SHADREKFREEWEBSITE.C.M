4e7417b5dbe7fa081b577aa79d43341bca3073ee
SHADRECKFREEWEBSITE <shadreckbanda3858@gmail.com>
2025-10-20 21:29:37 +0200

    Add files via upload

---
+++ ELISA/shad.ts
@@ -0,0 +1,19 @@
+<!doctype html>
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>form</title>
+</head>
+<body>
+<center><form>
+	surname :        kayuza <br>
+	first name :     maya<br>
+	date of birth<br>
+	denomination<br>
+	hobbies<br>
+	physical address<br>
+
+</form></center>
+</body>
+</html>
